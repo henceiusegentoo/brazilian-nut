0e978e71a0c59de4f5939f6f39edc81d6302a350
henceiusegentoo <61363222+henceiusegentoo@users.noreply.github.com>
2025-05-08 20:38:28 +0200

    Update build_and_deposit.sh to upgrade wheel installation and move shared objects

--- brazilian_nut_simulation/build_and_deposit.sh
+++ brazilian_nut_simulation/build_and_deposit.sh
@@ -1,3 +1,3 @@
 maturin build
-pip install --force-reinstall target/wheels/*.whl --target target/output/
-mv target/output/*.whl ../
+pip install --upgrade target/wheels/*.whl --target target/output/
+mv target/output/brazilian_nut_simulation/*.so ../brazilian_nut_wrapper/src/brazilian_nut_wrapper
