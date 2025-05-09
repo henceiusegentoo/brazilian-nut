0f944b0a98a3043c97d7ea3c91aef821aa610fee
henceiusegentoo <61363222+henceiusegentoo@users.noreply.github.com>
2025-05-09 03:17:51 +0200

    Enhance simulation execution by adding multiprocessing support and dynamic seed generation; update .gitignore to exclude PNG files

--- brazilian_nut_wrapper/__main__.py
+++ brazilian_nut_wrapper/__main__.py
@@ -1,22 +1,49 @@
+from typing import Optional
+
 from brazilian_nut_simulation import simulation # noqa | Pycharm can't look into the .so file so it fails to recognise the import and would throw an error at this point.
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 from scipy import stats
+from multiprocessing import Pool
 
 def main():
     particles = 50
     steps = 1_000_000
     temp = 1.5
-    seed = 0
 
-    res = np.array(simulation(
-        particles=particles,
-        steps=steps,
-        temp=temp,
-        seed=seed
-    ))
+    sim_count = 16
+
+    res_all = []
+
+    # generate sim_count seeds
+    seeds = [np.random.randint(0, 2**32 - 1) for _ in range(sim_count)]
+    seed_pairs = [(seeds[i], seeds[i + 1]) for i in range(0, sim_count - 1, 2)]
+
+    if sim_count % 2 != 0:
+        seed_pairs.append((seeds[-1],))
+
+    elif sim_count <= 0:
+        raise ValueError("Invalid number of simulations. Must be a positive integer.")
+
+    for seeds in seed_pairs:
+        try:
+            print(f"Running simulation with seeds {seeds}" if len(seeds) > 1 else f"Running simulation with seed {seeds[0]}")
+            with Pool() as pool:
+                res = pool.starmap(
+                    simulation_wrapper,
+                    [(particles, steps, temp, seed) for seed in seeds]
+                )
+
+            res = np.concatenate(res)
+            res_all.append(res)
+
+        except KeyboardInterrupt:
+            print("Simulation interrupted by user. Continuing with partial results.")
+            break
+
+    res = np.concatenate(res_all)
 
     loc, scale = stats.expon.fit(res)
 
@@ -50,6 +77,12 @@ def main():
 
     plt.savefig("brazilian_nut_simulation.png", dpi=300)
 
+    plt.clf()
+
+    print("Done!")
+
+def simulation_wrapper(particles, steps, temp, seed):
+    return np.array(simulation(particles, steps, temp, seed))
 
 if __name__ == "__main__":
     main()
